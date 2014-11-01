dbfb7a8f07f9f6d3a47b5c9719ff54b63db44b15
Marcus Povey <marcus@marcus-povey.co.uk>
2014-11-01 16:46:37 +0000

    Adding support for shortlink expansion. Depends on patch idno/idno#547

--- Bitly/Main.php
+++ Bitly/Main.php
@@ -6,7 +6,7 @@ namespace IdnoPlugins\Bitly {
 
 	public static $AUTHORIZATION_ENDPOINT = 'https://bitly.com/oauth/authorize';
 	public static $TOKEN_ENDPOINT = 'https://api-ssl.bitly.com/oauth/access_token';
-
+	
 	public static function getRedirectUrl() {
 	    return \Idno\Core\site()->config()->url . 'bitly/callback';
 	}
@@ -33,11 +33,16 @@ namespace IdnoPlugins\Bitly {
 	 * Can the current user use Bitly?
 	 * @return bool
 	 */
-	function hasBitly() {
+	function hasBitly($user) {
+	    if (!$user) $user=\Idno\Core\site()->session()->currentUser();
 	    if (\Idno\Core\site()->session()->currentUser()->bitly) {
 		return true;
 	    }
 	    return false;
+	}
+	
+	function hasGAT() {
+	    return isset(\Idno\Core\site()->config->config['bitly']['generic_access_token']);
 	}
 
 	function registerPages() {
@@ -52,7 +57,85 @@ namespace IdnoPlugins\Bitly {
 	    // Register the callback URL
 	    \Idno\Core\site()->addPageHandler('bitly/callback', '\IdnoPlugins\Bitly\Pages\Callback');
 
+	    // Listen to link text expand
+	    \Idno\Core\site()->addEventHook('url/expandintext', function(\Idno\Core\Event $event) {
+		
+		if ($object = $event->data()['object']) {
+		
+		    if ($owner = $object->getOwner()) {
+		
+			// See if the owner has bitly, or if we've got a global shortner, if not, don't bother
+			if ($this->hasBitly($owner) || $this->hasGAT()) {
+		
+			    // Get body
+			    $body = $event->response();
+			    
+			    // Load/create lookup table
+			    $urls = $object->url_expansion_lookup;
+			    if (!$urls) $urls = [];
+			    
+			    // Find urls
+			    if (preg_match_all('#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#', $body, $matches))
+			    {
+				foreach($matches[0] as $match) {
+				    
+				    $longurl = false;
+				    if (isset($urls[$match])) {
+					$longurl = $urls[$match];
+				    } else {
+					
+					// We haven't encountered this url, so try and expand it
+					
+					// Try user's bitly
+					if ($this->hasBitly($owner) ) { 
+					    $result = \Idno\Core\Webservice::get('https://api-ssl.bitly.com/v3/expand', [
+							'access_token' => $owner->bitly['access_token'],
+							'shortUrl' => $match
+					    ]);
 
+					    $result = json_decode($result['content']);
+					    if (isset($result->data->expand[0]->long_url))
+						$longurl = $result->data->expand[0]->long_url;
+					}
+					
+					// Now try GAT
+					if (!$longurl && $this->hasGAT()) {
+					    $result = \Idno\Core\Webservice::get('https://api-ssl.bitly.com/v3/expand', [
+							'access_token' => \Idno\Core\site()->config->config['bitly']['generic_access_token'],
+							'shortUrl' => $match
+					    ]);
+
+					    $result = json_decode($result['content']);
+					    if (isset($result->data->expand[0]->long_url))
+						$longurl = $result->data->expand[0]->long_url;
+					}
+				    }
+				    
+				    // Save expanded form, or that we failed.
+				    $urls[$match] = $longurl;
+				    
+				    // Now do a replace
+				    if ($longurl!==false) {
+					$body = str_replace($match, $longurl, $body);
+				    }
+				}
+			    }
+			    
+			    // Save updated urls list
+			    if (!empty($urls))
+			    {
+				$object->url_expansion_lookup = $urls;
+				$object->save();
+			    }
+			    
+			    // Update body
+			    $event->setResponse($body);
+			}
+		    
+		    }
+		}
+	    });
+	    
 	    // Handle shorten event
 	    \Idno\Core\site()->addEventHook('url/shorten', function(\Idno\Core\Event $event) {
 
